8c1029a5c3cf0f2549545d2f45b3fa418b2f3bfc
Joseph Han <josephkhhan@gmail.com>
2022-07-07 19:53:22 -0400

    ds store ignored

--- client/src/RatingsReviews/RatingsReviews.jsx
+++ client/src/RatingsReviews/RatingsReviews.jsx
@@ -1,3 +1,27 @@
+import React from "react";
+
+class RatingsReviews extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      reviews: []
+    }
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        Ratings and Reviews here.
+      </div>
+    )
+  }
+
+}
+
+export default RatingsReviews;
+
 //renders
 // ratings component on the left
 // reviewslist component on the right
