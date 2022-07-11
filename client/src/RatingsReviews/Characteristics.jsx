51cb49e351ae2de22528d43eded7ad03916c736f
Joseph Han <josephkhhan@gmail.com>
2022-07-11 17:48:14 -0400

    all characteristics showing

--- client/src/RatingsReviews/Characteristics.jsx
+++ client/src/RatingsReviews/Characteristics.jsx
@@ -6,7 +6,10 @@ function Characteristics(props) {
       {!props.isLoaded && <p>l o a d i n g . . .</p>}
       {props.isLoaded && (
         <div>
-          loaded.
+          Comfort: {props.meta.characteristics.Comfort.value}<br />
+          Length: {props.meta.characteristics.Length.value}<br />
+          Quality: {props.meta.characteristics.Quality.value}<br />
+          Fit: {props.meta.characteristics.Fit.value}
         </div>
       )}
     </div>
