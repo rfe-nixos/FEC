e145e4532398eed0bc630de722fd0c710914cc32
Joseph Han <josephkhhan@gmail.com>
2022-07-13 10:50:01 -0400

    styling

--- client/src/RatingsReviews/Reviews/SortBar.jsx
+++ client/src/RatingsReviews/Reviews/SortBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function SortBar(props) {
   const sort = (e) => {
@@ -6,14 +7,34 @@ function SortBar(props) {
   }
 
   return (
-    <div>
-      <b>{props.reviews.length} reviews, sorted by </b>
-      <select id="sort" onChange={sort}>
+    <StyledSortBar>
+
+      {props.reviews.length} reviews, sorted by
+      <StyledDropDown id="sort" onChange={sort}>
         <option value="relevance">Relevance</option>
         <option value="newest">Newest</option>
         <option value="helpful">Helpfulness</option>
-      </select>
-    </div>
+      </StyledDropDown>
+    </StyledSortBar>
   )
 }
+
+const StyledSortBar = styled.div`
+  display: flex;
+  font-weight: bold;
+  font-size: 15px;
+  align-
+`
+
+const StyledDropDown = styled.select`
+  margin-left: 2%;
+  font-weight: bold;
+  background: white;
+  font-size: 14px;
+  border: none;
+  border-bottom: 1px solid black;
+  align-self: flex-end;
+  margin-bottom: .4%;
+`
+
 export default SortBar;
