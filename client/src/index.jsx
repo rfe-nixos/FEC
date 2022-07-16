9c942561251947d66df5a829f37131f65bd6d7a3
Joseph Han <josephkhhan@gmail.com>
2022-07-16 11:55:57 -0400

    merge prep

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,9 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { Provider } from 'react-redux';
-import store from './store';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 
-const root = createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById('root'));
 
 root.render(
-    <App />,
-  document.getElementById('root')
+  <App />,
 );
