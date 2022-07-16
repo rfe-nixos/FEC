bd09b63b45e68ae4e21d9379f352a795286d6f1f
Joseph Han <josephkhhan@gmail.com>
2022-07-16 16:46:22 -0400

    merge conflict

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
-
-import Overview from './product-details/Overview';
+import styled from 'styled-components';
 import RatingsReviews from './RatingsReviews/RatingsReviews';
 import QuestionAnswer from './questionsAnswers/QuestionAnswers';
 import RelatedItems from './related-items/related-items.jsx';
 import Overview from './product-details/Overview.jsx';
 import { CurrentProductProvider } from './context.jsx';
 
-
 function App() {
   return (
-    <CurrentProductProvider>
-      <Overview />
-      <RelatedItems />
-      <QuestionAnswer productId={37311} />
-      <RatingsReviews product_id={37311} />
-    </CurrentProductProvider>
+    <MainDiv>
+      <CurrentProductProvider>
+        <Overview />
+        <RelatedItems />
+        <QuestionAnswer productId={37311} />
+        <RatingsReviews product_id={37311} />
+      </CurrentProductProvider>
+    </MainDiv>
   );
 }
+
+const MainDiv = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 export default App;
